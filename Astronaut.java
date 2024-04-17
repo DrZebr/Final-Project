2e91f6ea904835fa8845669bd7aed355568152fe
StevenSimon04 <000031317@student.baisd.net>
2024-04-17 08:47:26 -0400

    Astronaut.java

---
+++ Astronaut.java
@@ -0,0 +1,11 @@
+import java.util.jar.Attributes.Name;
+
+public class Astronaut {
+    String emailAddress;
+        String Name;
+        int dateOfBirth;
+        int serialNumber;
+        int phoneNumber;
+        int address;
+
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Nasa");
+        System.out.println("Please enter your Password");
+        
+
+    }
+
+}
